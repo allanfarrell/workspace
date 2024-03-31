186860f292b329fbd3f0766e92616c652299f562
Allan Farrell <farrella@deakin.edu.au>
2024-04-01 09:56:20 +1100

    rename app ubqx

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proto'`, () => {
+  it(`should have as title 'ubqx'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proto');
+    expect(app.title).toEqual('ubqx');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proto app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ubqx app is running!');
   });
 });
