700a452cd2505ea29cd4894c77e38590b63f890c
Allan Farrell <farrella@deakin.edu.au>
2024-05-11 13:07:22 +1000

    update release notes layout

--- src/app/modules/support/release-notes/release-notes.component.ts
+++ src/app/modules/support/release-notes/release-notes.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./release-notes.component.scss']
 })
 export class ReleaseNotesComponent {
-
+  data = [
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: '1', Version: '12.0', Date: '13th March 2023', Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }
+  ];
+  headers = [ 'Version', 'Date', 'Description' ];
 }
